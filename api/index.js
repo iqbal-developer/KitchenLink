8367e0215a9ebd8923a7d4eacc31dbefe2911e0a
Iqbal <sahiniqbal054@gmail.com>
2025-07-29 22:19:51 +0300

    Remove Vercel serverless setup for Railway

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// api/index.js
-const serverless = require('serverless-http');
-const app = require('../app');
-
-// Export as a serverless function
-module.exports = serverless(app);
-// This file is used to wrap the Express app for serverless deployment
-// It allows the app to be run in a serverless environment like AWS Lambda or Verc
