b0bfa7e72e1b5ce8646587a793321e75f98872e2
Iqbal <sahiniqbal054@gmail.com>
2025-08-06 19:55:58 +0300

    Remove old test files to restructure test suite

--- test/uptime.test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-setInterval(async () => {
-  try {
-    const res = await axios.get('http://localhost:3000');
-    console.log('UP', new Date());
-  } catch (e) {
-    console.log('DOWN', new Date());
-  }
-}, 60000); // every minute 
